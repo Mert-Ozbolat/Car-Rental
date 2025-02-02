35ce2430cfa5931bf2f1aa530e7eefd4c21b6eb3
Mert Özbolat <mertozbolat008@gmail.com>
2025-02-02 14:11:58 +0300

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react'
 import Hero from '../components/hero'
 import Filter from '../components/filter'
+import List from '../components/list'
 
 const Home: FC = () => {
     return (
         <div>
             <Hero />
             <Filter />
+            <List />
         </div>
     )
 }
