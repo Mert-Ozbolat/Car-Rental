9a23971b609b2e4995deb9a572f98d03cfd1d4a6
Mert Özbolat <mertozbolat008@gmail.com>
2025-02-02 15:20:17 +0300

    update

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,15 +1,38 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { ICar } from '../../types'
+import { fetchCars } from '../../utils/service'
+import { div } from 'motion/react-client'
+import Warning from './warning'
+
+
 
 const List: FC = () => {
 
 
-    useEffect(() => {
+    const [cars, setCars] = useState<ICar[]>([])
+    const [error, setError] = useState<string | null>(null)
 
+
+    useEffect(() => {
+        fetchCars()
+            .then((data) => setCars(data.results))
+            .catch((err) => setError(err.message))
     }, [])
 
+    if (!cars) return <Warning />
+    if (error) return <Warning />
+    if (cars.length < 1) return <Warning />
 
     return (
-        <div>List</div>
+        <div>
+            <section>
+                {
+                    cars.map((cars, i) => (
+                        <div key={i}>Kart</div>
+                    ))
+                }
+            </section>
+        </div>
     )
 }
 
