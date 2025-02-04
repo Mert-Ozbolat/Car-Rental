17141fbe0b09f53673332e6ecc18f62bd72471f6
Mert Özbolat <mertozbolat008@gmail.com>
2025-02-04 15:52:56 +0300

    update

--- src/components/filter/Fuel.tsx
+++ src/components/filter/Fuel.tsx
@@ -2,7 +2,14 @@ import { FC } from 'react'
 
 const Fuel: FC = () => {
     return (
-        <div>Select</div>
+        <form className='flex flex-col'>
+            <label htmlFor="">Yakıt</label>
+
+            <input type="string"
+                placeholder='Fuel Type'
+                className='w-24 py-[6px] px-2 rounded-[4px] shadow text-black bg-white'
+            />
+        </form>
     )
 }
 
